fff9db800da3a081c8f0b092e7ccf961f0c10778
harshascode <harshamesta0088@gmail.com>
2024-11-22 01:06:09 +0530

    ad code modifyed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,15 +17,15 @@
 	</div>
 </div>
 
-<!-- <svelte:head> -->
-	<!-- <script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://shebudriftaiter.net/tag.min.js',8505402,document.body||document.documentElement)</script> -->
-	<!-- <script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('reetahoo.com',8505412,document.createElement('script'))</script> -->
+<svelte:head>
+	<script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('reetahoo.com',8505412,document.createElement('script'))</script>
+	<script src="https://ptoutchoahensa.net/act/files/tag.min.js?z=8542847" data-cfasync="false" async></script>
 	<!-- <script
 		defer
 		data-website-id={import.meta.env.VITE_UMAMI_WEBSITE_ID}
 		src="https://umami-psi-three.vercel.app/script.js"
 	></script> -->
-<!-- </svelte:head> -->
+</svelte:head>
 
 <!-- <Analytics /> -->
 
